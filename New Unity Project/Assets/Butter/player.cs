1a2042ba4c7537c845cfcd6195ce3453f8f19d44
pointfour <lesha.seletskiy@gmail.com>
2018-06-03 15:25:40 -0700

    Woot everything

---
+++ New Unity Project/Assets/Butter/player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+
+    public Rigidbody2D rb;
+    private float sidespeed = 3;
+    public bool canjump = true;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.velocity = new Vector2(-sidespeed, rb.velocity.y);
+            Debug.Log("A" + rb.velocity);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            rb.velocity = new Vector2(sidespeed, rb.velocity.y);
+            Debug.Log("A" + rb.velocity);
+        }
+		
+    }
+}
---
+++ New Unity Project/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class PlayerMovement : MonoBehaviour {
+float timeStamp = 0f;
+	// Use this for initialization
+	void Start () {
+	timeStamp= Time.time;
+	print (transform.position);
+	}
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(Input.GetAxis("Horizontal")*Time.deltaTime*7f,Input.GetAxis("Jump")*Time.deltaTime*7f,0f);
+		if (timeStamp <= Time.time){
+			if (Input.GetAxis("Dimension") > 0){
+				if (transform.position.z<20){ 
+					transform.Translate(0f,0f,10f);
+					print(Input.GetAxis("Dimension"));
+					timeStamp = Time.time+2f;
+				}
+			}
+			if (Input.GetAxis("Dimension") < 0) {
+				if (transform.position.z>0) {
+					transform.Translate(0f,0f,-10f);
+					print(Input.GetAxis("Dimension"));
+					timeStamp = Time.time+2f;
+				}
+			}
+		}
+	}
+}
